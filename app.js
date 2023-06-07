ed6671ae39aac607976c3c4528c283e726b4b2c5
faizan dhedhi <120604695+muhammad-faizan-mohsin@users.noreply.github.com>
2023-06-07 19:16:05 +0500

    50% compelete

---
+++ app.js
@@ -0,0 +1,116 @@
+var questions = [
+  {
+    question  : "What does HTML stands for?",
+    option1:"Hypertext Machine language",
+    option2:"Hypertext and links markup language",
+    option3:"Hypertext Markup Language",
+    option4:"Hightext machine language",
+    correctAnswer: "Hypertext Markup Language"
+  },
+  {
+    question  : "How is document type initialized in HTML5?",
+    option1:"</DOCTYPE HTML>",
+    option2:"</DOCTYPE>",
+    option3:"<!DOCTYPE HTML>",
+    option4:"</DOCTYPE html>",
+    correctAnswer:"<!DOCTYPE HTML>",
+  },
+  {
+    question  : "Which of the following HTML Elements is used for making any text bold ?",
+    option1:"<p>",
+    option2:"<i>",
+    option3:"<li>",
+    option4:"<b>",
+    correctAnswer: "<b>"
+  },
+  {
+    question  : "What is the font-size of the h1 heading tag?",
+    option1:"3.5 em",
+    option2:"2.17 em",
+    option3:"2 em",
+    option4:"1.5 em",
+    correctAnswer: "2 em"
+  },
+  {
+    question  : "Which of the following HTML element is used for creating an unordered list?",
+    option1:"<ui>",
+    option2:"<i>",
+    option3:"<em>",
+    option4:"<ul>",
+    correctAnswer: "<ul>"
+  },
+  {
+    question  : "Which of the following characters indicate closing of a tag? ",
+    option1:".",
+    option2:"/",
+    option3:"//",
+    option4:"!",
+    correctAnswer: "/"
+  },
+  {
+    question  : "How many heading tags are there in HTML5?",
+    option1:"2",
+    option2:"3",
+    option3:"5",
+    option4:"6",
+    correctAnswer: "6"
+  },
+  {
+    question  : "How many attributes are there in HTML5?",
+    option1:"2",
+    option2:"4",
+    option3:"1",
+    option4:"None of the Above",
+    correctAnswer: "None of the Above"
+  },
+  {
+    question  : "Which of the following attributes is used to add link to any element?",
+    option1:"link",
+    option2:"ref",
+    option3:"href",
+    option4:"newref",
+    correctAnswer: "href"
+  },
+  {
+    question  : "<a>www.geeksforgeeks.org <Geeksforgeeks /a>",
+    option1:"<a href=“www.geeksforgeeks.org” Geeksforgeeks /a>",
+    option2:"Hypertext and links markup language",
+    option3:"<a href= “www.geeksforgeeks.org”>Geeksforgeeks</a>",
+    option4:"<a link=“www.geeksforgeeks.org” Geeksforgeeks> </a>",
+    correctAnswer: "<a href= “www.geeksforgeeks.org”>Geeksforgeeks</a>"
+  },
+]
+
+console.log(questions[index])
+// localStorage.setItem("email", "faizan@gmail.com");
+// localStorage.setItem("password", "faizan123");
+
+// function login(event) {
+//     event.preventDefault();
+//     var savedEmail = localStorage.getItem("email");
+//     var savedPassword = localStorage.getItem("password");
+  
+//     var enteredEmail = document.getElementById("email-input").value;
+//     var enteredPassword = document.getElementById("pass-input").value;
+
+//     if (enteredEmail === savedEmail && enteredPassword === savedPassword) {
+//         Swal.fire({
+//             icon: 'success',
+//             title: 'Logged in!',
+//             text: 'Wellcome to Quiz',
+//             showConfirmButton: false,
+//           });
+
+//           setTimeout(() => {
+//         location.assign("dashboard.html");
+//     }, 1000);
+      
+//     } else {
+//         Swal.fire({
+//             icon: 'error',
+//             text: 'Your email or password are incorrect ',
+//           });        }
+
+      
+//   }
+
